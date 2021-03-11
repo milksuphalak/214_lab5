c096f671ca981035937da797063243306e620c9c
milksuphalak <suphalak.l@ku.th>
2021-03-11 16:03:32 +0700

    1

--- 5.5.c
+++ 5.5.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int i,j;
-    int n,m;
+    int i,j,x,y;
+    int n,m,max=0,sum;
     scanf ("%d",&n);
     int a[n][n];
     scanf ("%d",&m);
-    int b[m][m],c;
     for(i=0;i<n;i++)
     {
         for(j=0;j<n;j++)
@@ -14,12 +13,27 @@ int main()
             scanf("%d",&a[i][j]);
         }
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<n-m+1;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<n-m+1;j++)
         {
 
-            printf ("%d",b[i][j]);
+            sum=0;
+            for (x=i;i<m+i;x++)
+            {
+                for (y=j;y<m+j;y++)
+                {
+                    sum+=a[x][y];
+                }
+
+            }
+        }
+        if (sum>max)
+        {
+            max=sum;
         }
     }
+    printf ("%d",sum);
+    printf ("%d",max);
+    return 0;
 }
